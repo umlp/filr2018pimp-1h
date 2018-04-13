f6c1332460d68840e37d201a54cb516a0e20aeea
tanya0703 <35974695+tanya0703@users.noreply.github.com>
2018-04-13 17:27:00 +0200

    Update tasks.blade.php

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -29,7 +29,7 @@
                         <div class="form-group">
                             <div class="col-sm-offset-3 col-sm-6">
                                 <button type="submit" class="btn btn-default">
-                                    <i class="fa fa-btn fa-plus"></i>HahaTask
+                                    <i class="fa fa-btn fa-plus"></i>Add Task
                                 </button>
                             </div>
                         </div>
