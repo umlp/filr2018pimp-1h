b1d48800641ba45ec3325acb94d83b7036a9ca8f
temion <thibaut.emion@insa-rouen.fr>
2018-05-22 13:48:29 +0200

    Essais Bootstrap

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -1,18 +1,21 @@
+<!-- col-xs col-sm col-md ol-lg -->
+
 @extends('layouts.app')
 
 @section('content')
-    <div class="row">
+    <div class="col-3">
         <div class="col-sm-12">
-            <p> ENTETE </p>
+            <p> Menu </p>
+        </div>
+        <div class="row">
+            <div class="col-sm-3">
+                <p> Barre gauche </p>
+            </div>
+            <div class="col-sm-9">
+                <p> Col B2 </p>
+            </div>
         </div>
     </div>
-    <div class="row">
-        <div class="col-sm-3">
-            <p> Barre gauche </p>
-        </div>
-        <div class="col-sm-9">
-            <p> Col B2 </p>
-        </div>
-    </div>
+
 
 @endsection
