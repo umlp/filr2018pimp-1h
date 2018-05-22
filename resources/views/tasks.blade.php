344b5c032fcbb6dfa4c7efe3d8a802d4f7bbaab3
temion <thibaut.emion@insa-rouen.fr>
2018-05-22 13:59:23 +0200

    Essais Bootstrap

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -5,10 +5,10 @@
 @section('content')
   <style>
       #lateral {
-          background-color: : red;
+          background-color: red;
       }
   </style>
-  
+
     <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
         <div id="lateral" class="col-sm-12">
             <p> Menu </p>
