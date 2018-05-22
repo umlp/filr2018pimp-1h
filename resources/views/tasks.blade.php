35956e8786c61e35022ae265e3ad1ab9dcc82d13
emiont <36051568+emiont@users.noreply.github.com>
2018-05-22 13:37:43 +0200

    Test Heroku

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -5,7 +5,7 @@
         <div class="col-sm-offset-2 col-sm-8">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    Nouvelle tâche
+                    Ploup test
                 </div>
 
                 <div class="panel-body">
