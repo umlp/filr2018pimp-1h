faaf7c702ef89c848a58f9faf749a0c6c3ded486
Di Gregorio Thomas <thomas.di_gregorio@insa-rouen.fr>
2018-05-23 22:59:22 +0200

    ajout map plein ecran

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -3,27 +3,25 @@
 @extends('layouts.app')
 
 @section('content')
-  <!-- <style>
-      #lateral {
-          background-color: red;
-      }
-  </style> -->
-<!--
-    <div id="lateral" class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-        <div class="col-sm-12">
-            <p> Menu </p>
-        </div>
-        <div class="row">
-            <div class="col-sm-3">
-                <p> Barre gauche </p>
-            </div>
-            <div class="col-sm-9">
-                <p> Col B2 </p>
-            </div>
-        </div>
-    </div> -->
+<div class="container">
+    <div id="maps"></div>
+</div>
 
-    <div id="map">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d41551.25820229445!2d1.0676016999999998!3d49.39086899999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sfr!4v1527093473772" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
-    </div>
+<footer>
+    <script>
+        $(function() {
+            initializeMap();
+        });
+
+        function initializeMap() {
+            var myOptions = {
+                center: new google.maps.LatLng(49.386078, 1.067933),
+                zoom: 14,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+
+            new google.maps.Map(document.getElementById("maps"), myOptions);
+        }
+    </script>
+</footer>
 @endsection
