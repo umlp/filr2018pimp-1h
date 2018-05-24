44394f29a39a8b62478cd76e0919b3d3f814f66d
temion <thibaut.emion@insa-rouen.fr>
2018-05-24 12:57:36 +0200

    Ajout image Zoé

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -7,7 +7,7 @@
     <iframe id="maps" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d9201.672545068184!2d1.0712783174099465!3d49.38828445157712!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sfr!4v1527110348924" frameborder="0" style="border:0" allowfullscreen></iframe>
 </div>
 <div id="infosZoe">
-  <img></img>
+  <img src="zoe.jpg" height="80%" width="10%"></img>
   <div>Zoé n° : X</div>
   <div>Vous rejoindra à : X</div>
   <div>Arrivée prévue à : X</div>
