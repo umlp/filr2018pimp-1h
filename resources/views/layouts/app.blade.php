19c29b94957d33e95741477ed7389ddf43a6ca69
Di Gregorio Thomas <thomas.di_gregorio@insa-rouen.fr>
2018-06-05 22:01:44 +0200

    alala cette navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,16 +22,28 @@
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </head>
 <body id="app-layout">
-<nav class="navbar navbar-inverse">
-  <div class="nav navbar-nav navbar-brand">AlloZoé</div>
+<nav class="navbar navbar-expand-lg navbar-inverse">
+  <div class="navbar-brand" href="#">AlloZoé</div>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
+
   <div class="collapse navbar-collapse" id="navbar">
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Créer un compte</a></li>
-      <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Se connecter</a></li>
+    <ul class="navbar-nav navbar-right mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
     </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
   </div>
 </nav>
     @yield('content')
