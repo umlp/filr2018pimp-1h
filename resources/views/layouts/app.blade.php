af174e1ad100f5009530f1f4b50e8843be515d27
Di Gregorio Thomas <thomas.di_gregorio@insa-rouen.fr>
2018-06-05 21:55:08 +0200

    oops y'avait deux navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,37 +22,6 @@
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </head>
 <body id="app-layout">
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar w/ text</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li>
-        <a href="#">
-          <span class="glyphicon glyphicon-user"></span> 
-          Créer un compte
-        </a>
-      </li>
-      <li>
-        <a href="login.php">
-          <span class="glyphicon glyphicon-log-in"></span> 
-          Se connecter
-        </a>
-      </li>
-    </ul>
-  </div>
-</nav>
 <nav class="navbar navbar-inverse">
   <div class="nav navbar-nav navbar-brand">AlloZoé</div>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
