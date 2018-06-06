b721e6a6f069233505efdb52fea7b159e24be808
temion <thibaut.emion@insa-rouen.fr>
2018-06-06 11:40:32 +0200

    Style

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -28,7 +28,7 @@
       </div>
       </div>
     </div>
-    <div id="divMap" class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
+    <div class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
       <iframe id="maps" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d9201.672545068184!2d1.0712783174099465!3d49.38828445157712!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sfr!4v1527110348924" frameborder="0" style="border:0" allowfullscreen></iframe>
     </div>
   </div>
