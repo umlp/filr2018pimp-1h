730c633783d3bf885b4a54fa1834d25a875740cb
Di Gregorio Thomas <thomas.di_gregorio@insa-rouen.fr>
2018-06-06 22:12:30 +0200

    Si ça marche…

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -1,6 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="modal fade" id="reserve" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalCenterTitle">Information</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Trajet réservé
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="modal fade" id="connexion" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalCenterTitle">Information</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Veuillez vous connecter pour réserver un trajet.
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Ferme</button>
+        <a class="btn btn-primary" href="{{ URL::to('login') }}" role="button">Se connecter</a>
+      </div>
+    </div>
+  </div>
+</div>
 <div class="container-fluid" id="main">
   <div class="row">
     <div id="criteres" class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
@@ -9,21 +46,21 @@
           <form method="post" action="#">
             <div class="form-group">
               <label for="depart">Adresse de départ :</label>
-              <input type="text" id="depart" class="form-control" name="depart" placeholder="Adresse de départ">
+              <input type="text" id="depart" class="form-control" name="depart" placeholder="Adresse de départ" required="required" />
             </div>
             <div class="form-group">
               <label for="destination">Destination :</label>
-              <input type="text" id="destination" class="form-control" name="destination" placeholder="Adresse de destination">
+              <input type="text" id="destination" class="form-control" name="destination" placeholder="Adresse de destination" required="required" />
             </div>
-            <input type="submit" class="btn btn-primary" name="submit" value="Réserver">
+            <input id="reserver" type="submit" class="btn btn-primary" name="submit" value="Réserver" />
           </form>
         </div>
       <div class="row">
         <img src="zoe.jpg" alt="Image de zoé" class="img-thumbnail" />
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-          <div>Zoé n° : X</div>
-          <div>Vous rejoindra à : X</div>
-          <div>Arrivée prévue à : X</div>
+          <div>Zoé n° : …</div>
+          <div>Vous rejoindra à : …</div>
+          <div>Arrivée prévue à : …</div>
         </div>
       </div>
       </div>
@@ -33,4 +70,14 @@
     </div>
   </div>
 </div>
+
+<script>
+  $(function() {
+    $("#reserver").on("click", function() {
+      <?php if(isset($utilisateur)) echo "$('#reserve').modal('show')";
+            else                    echo "$('#connexion').modal('show')";
+       ?>
+    });
+  })
+</script>
 @endsection
